ca86a06eae8b1fd4ed918dc7b7cf734eda3e7e95
Tung Nguyen <tung.nguyen@nbowree.com>
2026-02-24 17:45:20 +0700

    avoid deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -119,24 +119,37 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		// }
 
 		// Atomic SQL Update
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+		if arg.fromAccountID < arg.toAccountID {
+			result.FromAccount, from.ToAccount, err = addMoney(ctx, q, arg.fromAccountID, -arg.amount, arg.toAccountID, arg.amount)
+		} else {
+			result.ToAccount, from.FromAccount, err = addMoney(ctx, q, arg.toAccountID, arg.amount, arg.fromAccountID, -arg.amount)
 		}
 
 		return nil
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2
+	})
+	return
 }
