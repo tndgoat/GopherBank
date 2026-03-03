0b7c2fce1eca4c72e98ab481d381d0e377a2d7ad
Tung Nguyen <tung.nguyen@nbowree.com>
2026-03-03 11:06:27 +0700

    add create transfer api and currency validator

--- api/server.go
+++ api/server.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"github.com/gin-gonic/gin/binding"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	db	"github.com/tndgoat/gopherbank/db/sqlc"
 )
 
@@ -16,10 +18,16 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	// add routes to router
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
