daa87912bdc0967381b567040ccfddb432a41391
Tung Nguyen <tung.nguyen@nbowree.com>
2026-03-07 09:21:35 +0700

    fixxx

--- api/user_test.go
+++ api/user_test.go
@@ -11,8 +11,8 @@ import (
 	"reflect"
 	"testing"
 
+	"go.uber.org/mock/gomock"
 	"github.com/gin-gonic/gin"
-	"github.com/golang/mock/gomock"
 	"github.com/jackc/pgconn"
 	"github.com/stretchr/testify/require"
 	mockdb "github.com/tndgoat/gopherbank/db/mock"
